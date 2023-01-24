8dc99dcebcdb9c1b21ee6c5e1b41b8fa2b4de744
Gabriel U <ullmanng162@gmail.com>
2023-01-24 17:16:42 -0500

    adjust config for class

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        pathRewrite: {'^/api' : ''}
-      }
-    }
-  }
+  devServer: {}
 }
