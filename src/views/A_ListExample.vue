e2a190608814acc98c713e1d5230f1f13bf1cd77
Gabriel Ullmann <ullmanng162@gmail.com>
2023-03-09 11:40:17 -0500

    fix grocery list example

--- src/views/A_ListExample.vue
+++ src/views/A_ListExample.vue
@@ -1,27 +1,36 @@
 <template>
+  <div>
     <AWPList title="Grocery list" :list="myGroceryList"></AWPList>
+  </div>
 </template>
 
 <script>
-import AWPList from '@/components/AWPList.vue'
+import AWPList from "@/components/AWPList.vue";
 export default {
-    components: { AWPList },
-    name: 'AListExample',
-    data() {
-        return {
-            myGroceryList: [
-                { id: 1, name: 'bread' },
-                { id: 2, name: 'butter' },
-                { id: 3, name: 'apples' }
-            ]
-        }
+  components: { AWPList },
+  name: "AListExample",
+  data() {
+    return {
+      myGroceryList: ["apple", "banana", "bread"]
+    };
+  },
+  methods: {
+    getList: function () {
+      // use the fetch function to request data (GET) from the server
+      // wait for an answer, it will be a string
+      // convert the string to array
+      // update the array on the component
     },
-    methods: {
-        getList: function() {
-            // make a fetch request to back-end
-            // wait for response, get the data
-            // pass it to the component
-        }
-    } 
-}
+    add: function () {
+      // use the fetch function to send data (POST) to the server
+      // wait for an answer, it will be a string
+      // convert the string to array
+      // update the array on the component
+    },
+  },
+  beforeMount() {
+    // this will execute when the app starts
+    // call my getList method, it will read the list from the server
+  },
+};
 </script>
